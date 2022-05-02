2f681c67acec7e7a83f0b7192d0e32da1781762a
atlehjelmseth <atle.hjelmseth@stud.noroff.no>
2022-05-02 17:59:59 +0200

    Update

---
+++ question2.js
@@ -0,0 +1,22 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=b5350ec8392042dabf67dca0cb915e15"
+
+const gameContainer = document.querySelector(".game-container");
+
+async function getGameApi() {
+      try{ const response = await fetch(url);
+       const results = await response.json();
+       const facts = results.results;
+      
+       for(let i = 0; i < facts.length; i++) {
+         if (i === 8) {
+           break;
+         }
+         console.log(facts)
+         gameContainer.innerHTML += `<div class="text">Name: ${facts[i].name} - Rating: ${facts[i].rating} - Tags: ${facts[i].tags.length}</div>`;
+       }
+      }catch (error) {
+        gameContainer.innerHTML = "Unable to connect to the API";
+    }
+}
+
+getGameApi()
